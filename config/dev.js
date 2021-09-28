645f2bba8b99dfc16688b3c26b6c331676bd1488
sjt1379 <1004sjt52@gmail.com>
2021-09-28 20:28:51 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI: 'mongodb+srv://shin:1379@cluster0.tnicd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require('./config/key');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할떄 필요한 정보들을  client에서 가져오면 
+    //그것들을  데이터 베이스에 넣어준다. 
+    const user = new User(req.body)
+  
+    user.save((err, userInfo) => {
+      if (err) return res.json({ success: false, err })
+      return res.status(200).json({
+        success: true
+      })
+    })
+})
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://shin:1379@cluster0.tnicd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
+
